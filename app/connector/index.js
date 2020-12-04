7fbd3511f9912a3f55da8b382dfa99d3a1225f84
myhlcb <1029785965@qq.com>
2020-12-04 10:58:19 +0800

    feat(docker-compose): add docker mongo

--- app/connector/index.js
+++ app/connector/index.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
+const config = require('config')
 const {
   mongodb: { host, port, database },
-} = require('../../config/');
-
-const db = mongoose.createConnection(`mongodb://${host}:${port}/${database}`);
+} = config
+const url = process.env['MONGO_URL'] || `mongodb://${host}:${port}/${database}`
+const db = mongoose.createConnection(url);
 console.log(`get mongodb uri: mongodb://${host}:${port}/${database}`);
 db.on('connected', () => {
   console.log('connect success');
